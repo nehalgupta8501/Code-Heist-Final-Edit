fc5e6ec568589665db7e9f5ee518a1b421ce125f
Ipshita Diwan <24778311+hoover-phonic@users.noreply.github.com>
2020-10-26 16:27:24 +0530

    Corrected the issues with login page

--- login.js
+++ login.js
@@ -1,48 +1,47 @@
-function signIn(){
+function signIn() {
   var Email = document.getElementById("email").value;
   var Password = document.getElementById("password").value;
   // var TeamName = document.getElementById("tname").value;
 
   // if(TeamName == null){
   //       window.location.replace("error.html");
-  // }else 
-  if(Email == null){
-        window.location.replace("error.html");
-  }else if(Password  == null){
-        window.location.replace("error.html");
-  }else{
-      var ref = firebase.database().ref("users");
-      ref.orderByChild("email").equalTo(Email).on("value", function(snapshot) {
-        
-        if(snapshot.exists()){
-          
-          snapshot.forEach(function(childSnapshot) {
+  // }else
+  if (Email === "") {
+    // window.location.replace("error.html");
+    alert("Enter your email id!");
+  } else if (Password === "") {
+    // window.location.replace("error.html");
+    alert("Enter your passoword!");
+  } else {
+    var ref = firebase.database().ref("users");
+    ref
+      .orderByChild("email")
+      .equalTo(Email)
+      .on("value", function (snapshot) {
+        if (snapshot.exists()) {
+          snapshot.forEach(function (childSnapshot) {
             const temp = childSnapshot.val();
-             if(temp.password == Password){
-                    //Store in LocalStorage
-                    storeCredsInLocalStorage(childSnapshot.key);
+            if (temp.password == Password) {
+              //Store in LocalStorage
+              storeCredsInLocalStorage(childSnapshot.key);
 
-
-                    window.location.replace("game_intro.html");
-                  }
+              window.location.replace("game_intro.html");
+            } else {
+              window.location.replace("error.html");
+            }
           });
-        }
-        else {
-         window.location.replace("error.html"); 
+        } else {
+          window.location.replace("error.html");
         }
       });
-    }
   }
+}
 
-  //Stores User Credentials
-  function storeCredsInLocalStorage(key){
-    let id;
-    localStorage.setItem('id', JSON.stringify(key));
-  }
-
-
-
-
+//Stores User Credentials
+function storeCredsInLocalStorage(key) {
+  let id;
+  localStorage.setItem("id", JSON.stringify(key));
+}
 
 /*
       ref.orderByChild('email').equalTo(teamEmail).on("value", function(snapshot) {
