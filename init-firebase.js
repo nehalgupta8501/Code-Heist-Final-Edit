1e94f6bc724f23c36e94414a833787c96ee601f6
gama626 <55777739+gama626@users.noreply.github.com>
2020-11-05 14:25:30 +0530

    Sensitive code obfuscated

--- init-firebase.js
+++ init-firebase.js
@@ -1,14 +1,1 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCwMRLfym-QiGiGKDLGdFbMfVVNs0Bcy6M",
-    authDomain: "firebasics-b4d13.firebaseapp.com",
-    databaseURL: "https://firebasics-b4d13.firebaseio.com",
-    projectId: "firebasics-b4d13",
-    storageBucket: "firebasics-b4d13.appspot.com",
-    messagingSenderId: "529517682921",
-    appId: "1:529517682921:web:547cbcf81172b7ce1a5078",
-    measurementId: "G-Y4RT0LR8HV"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+var _0x5c9a=['firebasics-b4d13.firebaseapp.com','analytics','G-Y4RT0LR8HV','firebasics-b4d13','firebasics-b4d13.appspot.com','529517682921','1:529517682921:web:547cbcf81172b7ce1a5078'];(function(_0x5b8a89,_0x5c9a00){var _0xc5c6ae=function(_0x59593d){while(--_0x59593d){_0x5b8a89['push'](_0x5b8a89['shift']());}};_0xc5c6ae(++_0x5c9a00);}(_0x5c9a,0x1b1));var _0xc5c6=function(_0x5b8a89,_0x5c9a00){_0x5b8a89=_0x5b8a89-0x0;var _0xc5c6ae=_0x5c9a[_0x5b8a89];return _0xc5c6ae;};var _0x2aa834=_0xc5c6,firebaseConfig={'apiKey':'AIzaSyCwMRLfym-QiGiGKDLGdFbMfVVNs0Bcy6M','authDomain':_0x2aa834('0x1'),'databaseURL':'https://firebasics-b4d13.firebaseio.com','projectId':_0x2aa834('0x4'),'storageBucket':_0x2aa834('0x5'),'messagingSenderId':_0x2aa834('0x6'),'appId':_0x2aa834('0x0'),'measurementId':_0x2aa834('0x3')};firebase['initializeApp'](firebaseConfig),firebase[_0x2aa834('0x2')]();
